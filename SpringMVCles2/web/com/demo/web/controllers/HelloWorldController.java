be6c1f1ec38c57b0a018377879297d1701061c85
wangyuchen <chen_841460210@qq.com>
2017-02-18 15:19:07 +0800

    hellow and id=123

--- SpringMVCles2/web/com/demo/web/controllers/HelloWorldController.java
+++ SpringMVCles2/web/com/demo/web/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.demo.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,15 @@ public class HelloWorldController {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("message", "Hello World!");
 		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/detail/{id}", method = { RequestMethod.GET })
+	public ModelAndView getDetail(@PathVariable(value = "id") Integer id) {
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("id", id);
+		modelAndView.setViewName("detail");
 		return modelAndView;
 	}
 
